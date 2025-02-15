4f6fe8176337a9389e82537b65156a83d13d6599
InnaBondarenko87 <bondarenko211287@gmail.com>
2025-02-15 22:07:46 +0100

    add
    2 lesson

--- js/lesson-2.js
+++ js/lesson-2.js
@@ -11,11 +11,11 @@
 
 // const styles = ["jazz", "blues"];
 
-// const rock = styles.concat(["rock-n-roll"]);
-
-// console.log(rock.indexOf("blues"));
-
-// rock[1] = "classic";
+// styles.push("rock-n-roll");
+// const index = styles.indexOf("blues");
+// if (index !== -1) {
+//   styles.splice(index, 1, "classic");
+// }
 
 // function logItems(array) {
 //   for (let i = 0; i < array.length; i++) {
@@ -23,7 +23,7 @@
 //   }
 // }
 
-// logItems(rock);
+// logItems(styles);
 
 // ************** 2 *************
 
@@ -36,19 +36,31 @@
 
 // const logins = ["Peter", "John", "Igor", "Sasha"];
 
-// function checkLogin(debil) {
-//   const nameLogins = prompt("ВВЕДІТЬ ІМЯ КОРИСТУВАЧА:").trim();
-//   //   console.log(debil);
-//   for (const login of debil) {
-//     if (login === nameLogins) {
+// function checkLogin(array) {
+//   const nameLogins = prompt("ВВЕДІТЬ ІМЯ КОРИСТУВАЧА:");
+//   if (!nameLogins) {
+//     return alert("jknm");
+//   }
+//   const normalizeName = nameLogins.toLowerCase().trim();
+//   for (const login of array) {
+//     if (login.toLowerCase() === normalizeName) {
 //       return alert(`"Welcome, ${login}!"`);
-//     } else {
-//       return alert("User not found");
 //     }
 //   }
+//
+// return alert("User not found");
 // }
 
 // checkLogin(logins);
+
+// II
+
+// function checkLogin(array) {
+//     let (array.includes(userName)){
+// alert('Welkom, ${'userName)
+//     }
+// }
+
 // *************************
 
 // ******   3   ***********
@@ -59,8 +71,7 @@
 
 // function caclculateAverage() {
 //   const args = Array.from(arguments);
-//   //
-//   //   console.log(args);
+
 //   let sum = 0;
 
 //   for (let i = 0; i < args.length; i++) {
@@ -75,20 +86,22 @@
 // console.log(caclculateAverage(1, 2, "sfsd", "dsfdsfg"));
 // console.log(caclculateAverage(1, 20, 30, 54));
 
-// без перевірки!!!
-// functifunction caclculateAverage() {
-//   const args = Array.from(arguments);
-//   //   console.log(args);
-//   let sum = 0;
+// PRACTIK     ************
 
-//   for (let i = 0; i < args.length; i++) {
-//     sum += args[i];
+// function caclculateAverage(...args) {
+//   let sum = 0;
+//   let count = 0;
+//   for (const arg of args) {
+//     if (typeof arg === "number") {
+//       sum += arg;
+//       count += 1;
+//     }
 //   }
-//   return sum / args.length;
+
+//   return sum / count;
 // }
 
-// console.log(caclculateAverage(1, 2, 3));
-// console.log(caclculateAverage(1, 20, 30, 54));
+// console.log(caclculateAverage(1, 3, 17, true));
 // ***********************************************
 
 // ********    4   ******
@@ -122,25 +135,27 @@
 // якщо функція отримує масив - поверніть з функції найменше число,
 // в іншому випадку - поверніть 'Sory, it is not an array!'.
 
+// *************  Practik  **************
+
 // const numbers = [2, 5, 35, 56, 12, 24, 7, 80, 3];
 
 // function findSmallestNumber(numbers) {
-//   let min;
-//   //   console.log(numbers);
-//   for (
-//     let i = 0;
-//     i < numbers.length;
-//     i++ // console.log(numbers[i]);
-//   ) {
-//     if (numbers[i] < numbers[i + 1]) {
-//       min = numbers[i];
-//     } else {
-//       min = numbers[i + 1];
+//   if (!Array.isArray(numbers)) {
+//     return "Sory, it is not an array!";
+//   }
+//   let SmallestNumber = numbers[0];
+//   for (const number of numbers) {
+//     if (number < SmallestNumber) {
+//       SmallestNumber = number;
 //     }
 //   }
-//   //   return min;
+
+//   return SmallestNumber;
 // }
-// findSmallestNumber(numbers);
+
+// console.log(findSmallestNumber(numbers));
+
+//************ II *************
 
 // function findSmallestNumber(numbers) {
 //   let min;
@@ -157,7 +172,8 @@
 // }
 // findSmallestNumber(numbers);
 
-// Lesson
+//
+// ************  Lesson III ***********
 // const values = [-24, 17, 94, 1, -23, 37];
 
 // function findSmallestNumber(numbers) {
@@ -178,7 +194,7 @@
 // console.log(findSmallestNumber(values));
 // *********************************************
 
-// ********   6    ******
+// ********   6    ******??????????/
 // Напишіть функцію findLongestWord(string), яка
 // приймає довільний рядок, що складається лише зі слів, розділених
 // пробілами (параметр string), і повертатиме найдовше слово у реченні.
@@ -186,15 +202,37 @@
 // Скористайтесь цим прикладом виклику функції для перевірки її роботи:
 // console.log(findLongestWord("London is the capital of Great Britain")); // 'capital'
 
-function findLongestWord() {
-  const args = Array.from(arguments).join(" ");
-  console.log(args);
+// I
+// function findLongestWord(string) {
+//   let str;
+//   //   console.log(str);
+// }
 
-  for (let i = 0; i < args.length; i++) {}
-}
+// console.log(findLongestWord("tooo kjk laaa"));
 
-findLongestWord(1, 2, 3);
-findLongestWord(10, 20, 30, 40);
+// console.log(findLongestWord("London is the capital of Great Britain")); // 'capital'
+
+// II
+// function findLongestWord(string) {
+//   //   const args = Array.from(arguments).join(" ");
+//   //   console.log(args);
+//   let arr = [];
+//   let b = Math.max(...arr).indexOf();
+//   const str = string.split(" ");
+
+// //   console.log(arr);
+
+//   for (const item of str) {
+//     const a = item.split("").length;
+//     //   console.log(a);
+//     arr.push(a);
+//   }
+//   //   return
+// }
+
+// findLongestWord(1, 2, 3);
+// findLongestWord(10, 20, 30, 40);
+// console.log(findLongestWord("London is the capital of Great Britain"));
 // console.log(findLongestWord("London is the capital of Great Britain"));
 // **************************************************************************
 
@@ -293,11 +331,59 @@ findLongestWord(10, 20, 30, 40);
 // Створіть об'єкт calculator з наступними методами:
 // read(a, b) - приймає два аргумента і зберігає їх як властивості об'єкта,
 // sum() - повертає сумму збереженних значень (з перевіркою на наявність властивостей в об'єкті),
-// mult() - перемножає збереженні значення і повертає результат (з перевіркою на наявність властивостей в об'єкті),
+// mult() - перемножає збереженні значення і повертає результат (з перевіркою на наявність
+// властивостей в об'єкті),
 // винесіть перевірку на наявність властивостей в об'єкті в окремий метод exist().
 
 // Якщо вказані властивості в обʼєкті відсутні (тобто метод exist повертає false),
 // методи sum і mult мають повертати рядок 'No such propeties'
+
+// calculator = {
+//   read(a, b) {
+//     this.a = a;
+//     this.b = b;
+//   },
+//   exist() {
+//     return this.a !== undefined && this.b !== undefined;
+//   },
+//   sum() {
+//     return this.exist() ? this.a + this.b : "No such propeties";
+//   },
+//   mult() {
+//     return this.exist() ? this.a * this.b : "No such propeties";
+//   },
+// };
+
+// calculator.read(1, 5);
+// console.log(calculator.exist());
+// console.log(calculator.mult());
+
+// II***********
+
+// calculator = {
+//   read(a, b) {
+//     this.a = a;
+//     this.b = b;
+//   },
+//   exist() {
+//     if (this.a !== undefined && this.b !== undefined) {
+//       return this.sum;
+//     } else {
+//       return "No such propeties";
+//     }
+//   },
+//   sum() {
+//     return this.a + this.b;
+//   },
+//   mult() {
+//     return this.a * this.b;
+//   },
+// };
+
+// calculator.read(2, 5);
+// console.log(calculator.sum());
+// console.log(calculator.mult());
+// console.log(calculator.exist());
 
 // ******************
 
@@ -311,10 +397,26 @@ findLongestWord(10, 20, 30, 40);
 // назвою фрукта, це також треба урахувати.
 
 // const fruits = [
-//     { name: "Яблуко", price: 45, quantity: 7 },
-//     { name: "Апельсин", price: 60, quantity: 4 },
-//     { name: "Банан", price: 125, quantity: 8 },
-//     { name: "Груша", price: 350, quantity: 2 },
-//     { name: "Виноград", price: 440, quantity: 3 },
-//     { name: "Банан", price: 125, quantity: 3 },
-//   ];
+//   { name: "Яблуко", price: 45, quantity: 7 },
+//   { name: "Апельсин", price: 60, quantity: 4 },
+//   { name: "Банан", price: 125, quantity: 8 },
+//   { name: "Груша", price: 350, quantity: 2 },
+//   { name: "Виноград", price: 440, quantity: 3 },
+//   { name: "Банан", price: 125, quantity: 3 },
+// ];
+
+// function calcTotalPrice(fruits, fruitName) {
+//   let sum = 0;
+//   for (const item of fruits) {
+//     if (item.name === fruitName) {
+//       sum += item.price * item.quantity;
+//     }
+//   }
+//   console.log(`${fruitName}: ${sum}`);
+// }
+
+// calcTotalPrice(fruits, "Яблуко");
+// calcTotalPrice(fruits, "Банан");
+
+// console.log(calcTotalPrice(fruits, "Яблуко"));
+// console.log(calcTotalPrice(fruits, "Банан"));
