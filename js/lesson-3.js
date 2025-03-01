d40aa7e339964ae7cc04251c02990790aecefcff
InnaBondarenko87 <bondarenko211287@gmail.com>
2025-03-01 23:01:33 +0100

    add

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -8,6 +8,7 @@
 // const newNumbers = numbers.map((number) => number ** 2);
 
 // console.log(newNumbers);
+
 // ****************************************
 // / ****************************************
 
@@ -79,7 +80,7 @@
 // Відсортуйте масив чисел [4, 2, 5, 1, 3]
 // у порядку зростання. Очікуваний результат: [1, 2, 3, 4, 5].
 
-// const numbersArray = [4, 2, 5, 1, 3];
+// const numbersArray = [45, 2, 5, 1, 33];
 
 // const newNumbersArray = numbersArray.toSorted((a, b) => a - b);
 
@@ -98,7 +99,7 @@
 // console.log(newStringArray);
 // *****************************************
 
-// Завдання 8:????????????????????????????
+// Завдання 8:
 
 //  Відсортуйте масив об'єктів за віком у порядку зростання.
 // Очікуваний результат: [{name: "Bob", age: 19}, {name: "John", age: 27}, {name: "Jane", age: 31}].
@@ -162,15 +163,15 @@
 // Об'єкт класу може проводити послідовні операції у вигляді ланцюжка
 
 // class Calc {
-//     constructor()
-//  number() {
-//         const num = 0,
+
+//  number(value) {
+//        this.num = value.num
 //  }
 //     getResult() {
-
+// return this.resalt
 //     };
-//     add() {
-//         num + fggh;
+//     add(value) {
+//         this.num +
 //     };
 
 //     substruct() {
@@ -182,8 +183,9 @@
 //     multiply() {
 
 //     }
-
 // }
+
+// const result = new Calc;
 
 // Завдання 12:
 // Напиши клас Client який створює об'єкт з властивостями login email.
@@ -219,8 +221,8 @@
 // console.log(inna.getEmail());
 // console.log(inna.getLogin());
 
-// // inna.setLogin("Inna SUPER!!!");
-// // console.log(inna.setLogin());
+// inna.setLogin("Inna SUPER!!!");
+// console.log(inna.getLogin());
 
 // inna.setEmail("super_inna@gmail.com");
 // console.log(inna.getEmail());
@@ -244,3 +246,147 @@
 //  - salary - зарплата співробітника;
 //  - department - відділ, в якому працює співробітник.
 //  ##### Крім того, клас `Employee` має мати метод `getEmployeeDetails()`, який повертає об'єкт з ідентифікатором співробітника, зарплатою та відділом, в якому працює співробітник.
+
+// class Person {
+//   constructor(obj) {
+//     this.name = obj.name;
+//     this.age = obj.age;
+//     this.gender = obj.gender;
+//     this.email = obj.email;
+//   }
+//   getDetails() {
+//     return {
+//       name: this.name,
+//       age: this.age,
+//       gender: this.gender,
+//       email: this.email,
+//     };
+//   }
+// }
+
+// class Employee extends Person {
+//   constructor(obj) {
+//     super({
+//       name: obj.name,
+//       age: obj.age,
+//       gender: obj.gender,
+//       email: obj.email,
+//     });
+//     this.salary = obj.salary;
+//     this.department = obj.department;
+//   }
+
+//   getEmployeeDetails() {
+//     return { salary: this.salary, department: this.department };
+//   }
+// }
+
+// const person1 = new Person({
+//   name: "Arina",
+//   age: 20,
+//   gender: "female",
+//   email: "ari@gmail.com",
+// });
+// console.log(person1);
+
+// const pracownik = new Employee({
+//   name: "Inna",
+//   age: 32,
+//   gender: "female",
+//   email: "inna@gmail.com",
+//   salary: 10000,
+//   department: "logistyka",
+// });
+
+// console.log(pracownik);
+
+// ================
+
+// ========================
+
+// ========================
+
+// const users = [
+//   {
+//     id: "701b29c3-b35d-4cf1-a5f6-8b12b29a5081",
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     skills: ["ipsum", "lorem"],
+//     gender: "male",
+//     age: 37,
+//   },
+//   {
+//     id: "7a3cbd18-57a1-4534-8e12-1caad921bda1",
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     skills: ["tempor", "mollit", "commodo", "veniam", "laborum"],
+//     gender: "female",
+//     age: 34,
+//   },
+//   {
+//     id: "88beb2f3-e4c2-49f3-a0a0-ecf957a95af3",
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     skills: ["nulla", "anim", "proident", "ipsum", "elit"],
+//     gender: "male",
+//     age: 24,
+//   },
+//   {
+//     id: "249b6175-5c30-44c6-b154-f120923736f5",
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     skills: ["adipisicing", "irure", "velit"],
+//     gender: "female",
+//     age: 21,
+//   },
+//   {
+//     id: "e1bf46ab-7168-491e-925e-f01e21394812",
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     skills: ["lorem", "veniam", "culpa"],
+//     gender: "female",
+//     age: 39,
+//   },
+// ];
+
+// const userGenderAge = (users, gender, age) =>
+//   users.reduce((acc, user) => {
+//     if (user.gender === gender && user.age <= age) {
+//       acc.push(user.name);
+//     }
+
+//     return acc;
+//   }, []);
+
+// console.log(userGenderAge(users, "female", 30));
+// console.log(userGenderAge(users, "male", 40));
+
+// ===================================================
+
+// arr = [4, 23, 5, 4, 17, 5, 45];
+
+// unique = arr.filter((item, index, arr) => arr.indexOf(item) === index);
+
+// console.log(unique);
+
+// =======================================================
